e6dd2766f62955f128185c6765235e5855912672
FairyZeta <fairyzeta@kimamani.jp>
2016-12-21 00:21:01 +0900

    プロジェクト ファイルを追加します。

---
+++ Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FairyZeta.Generics.PCL.SQLite
+{
+    public class Class1
+    {
+    }
+}
